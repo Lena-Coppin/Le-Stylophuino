fc94292aae22030cae38de537200306bbf27bed9
Julien Noel <95232152+Julien-Noel@users.noreply.github.com>
2022-01-23 00:04:36 +0100

    Add files via upload

---
+++ LeCode/code_fred_testons/aled.h
@@ -0,0 +1,16 @@
+/*********************************************************************
+ *       On retrouve ici tout ce qui gère le système de LED.
+*********************************************************************/
+
+
+int where_led_on_off = 13;
+int where_led_B_b = 12;
+int where_led_G_b = 11;
+int where_led_R_b = 10;
+int where_led_B_m = 9;
+int where_led_G_m = 8;
+int where_led_R_m = 7;
+int where_led_bend = 6;
+int where_led_tempo = 5;
+int where_led_recorded = 4;
+int where_led_rhythm = 3;
